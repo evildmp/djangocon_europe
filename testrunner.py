576dcce512949a445a0cd5697d39bb6350e5cddf
Daniele Procida <daniele@apple-juice.co.uk>
2014-11-26 20:37:55 +0100

    initial commit

---
+++ testrunner.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from django.core import management
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangocon_europe.test_settings")
+
+    management.call_command(
+        'test',
+        'arkestra_utilities',
+        'contacts_and_people',
+        'news_and_events',
+        'vacancies_and_studentships',
+        'links',
+        # verbosity=2,
+        )
